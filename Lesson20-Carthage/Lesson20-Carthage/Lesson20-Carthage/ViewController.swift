9065ac63cc34b769ae9e21f6bb0b5b616107f9a7
Vladislav Galkin <cascascas303@gmail.com>
2021-06-10 22:27:02 +0300

    Lesson20-Carthage

---
+++ Lesson20-Carthage/Lesson20-Carthage/Lesson20-Carthage/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Lesson20-Carthage
+//
+//  Created by Владислав Галкин on 10.06.2021.
+//
+
+import UIKit
+import fraemworkSberCarthage
+
+class ViewController: GSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeBackground(withColor: .red)
+    }
+}
+
